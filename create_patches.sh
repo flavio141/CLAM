7e285843f153eb49302a461fda47ea5a33b0fa8c
flavi141 <flavio.sartori@edu.unito.it>
2023-03-15 16:02:57 +0000

    Created bash scripts and installed libraries

---
+++ create_patches.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#PARTE 1: Elenco delle cartelle da cancellare
+folders=(
+    FEATURES_DIRECTORY
+    RESULTS_DIRECTORY
+)
+
+# Ciclo su tutte le cartelle da cancellare
+for folder in "${folders[@]}"
+do
+    if [ -d "$folder" ]; then
+        # Cancella la cartella e tutte le sue sottocartelle
+        echo "Cancellazione delle subfolders di $folder..."
+        rm -rf $folder/*
+    else
+        echo "Attenzione: la cartella $folder non esiste."
+    fi
+done
+
+#PARTE 2: Create le Patches
+if [ -z "$(ls -A DATA_DIRECTORY)" ]; then
+    echo "Non esistono dati in DATA_DIRECTORY"
+else
+    python create_patches_fp.py --source DATA_DIRECTORY --save_dir RESULTS_DIRECTORY --patch_size 256 --preset bwh_biopsy.csv --seg --patch --stitch
+fi
